d25ac20b7a6d2f992caad9818b37ca38a5a089cd
zahovic Saravia Suarez <zv.elrevelde@gmail.com>
2021-10-24 23:05:14 -0500

    fixed something

---
+++ components/ui/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-blue-400">
+    <span class="text-white">Holaa</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Navbar />
+    <Nuxt />
+  </div>
+</template>
+<script>
+import Navbar from '../components/ui/Navbar.vue'
+export default {
+  components: {
+    Navbar,
+  },
+}
+</script>
